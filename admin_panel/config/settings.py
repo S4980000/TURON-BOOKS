4855b3927ef1447cd02c83d5cebbab8ed3e66c38
aymogo <imshalch4309@gmail.com>
2025-04-22 20:29:11 +0500

    add: Django models, fix: .gitignore and settings file

--- admin_panel/config/settings.py
+++ admin_panel/config/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # apps
     "library",
-    "django-environ",
 ]
 
 MIDDLEWARE = [
--- admin_panel/library/models.py
+++ admin_panel/library/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+nb = dict(null=True, blank=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='children', **nb)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='books')
+    file_id = models.CharField(max_length=512)
+    caption = models.TextField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.pk} {self.caption[:25]}'
